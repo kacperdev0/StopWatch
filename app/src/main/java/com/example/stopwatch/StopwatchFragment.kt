9925980aa0c0a583774de77a1625a7fef7a9bc63
KacperOldz <kobula@protonmail.com>
2024-05-19 15:25:23 +0200

    added navigation between stopwatch and timer

---
+++ app/src/main/java/com/example/stopwatch/StopwatchFragment.kt
@@ -0,0 +1,109 @@
+package com.example.stopwatch
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StopwatchFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StopwatchFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val startButton = view.findViewById<Button>(R.id.start_button)
+        val stopButton = view.findViewById<Button>(R.id.stop_button)
+        val resetButton = view.findViewById<Button>(R.id.reset_button)
+        val stopwatchText = view.findViewById<TextView>(R.id.counter)
+
+        var startTime = 0L
+        var offset = 0L
+
+        var handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            override fun run() {
+                var time = System.currentTimeMillis() - startTime
+                stopwatchText.text = formatTime(time + offset)
+                handler.postDelayed(this, 100)
+            }
+        }
+
+        startButton.setOnClickListener {
+            startTime = System.currentTimeMillis()
+            handler.post(runnable)
+        }
+
+        stopButton.setOnClickListener {
+            offset += System.currentTimeMillis() - startTime
+            handler.removeCallbacks(runnable)
+        }
+
+        resetButton.setOnClickListener {
+            startTime = 0L
+            offset = 0L
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun formatTime(elapsedTime: Long): String {
+        val milliseconds = elapsedTime % 1000
+        val seconds = (elapsedTime / 1000) % 60
+        val minutes = (elapsedTime / (1000 * 60)) % 60
+        val hours = (elapsedTime / (1000 * 60 * 60)) % 24
+        return String.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stopwatch, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StopwatchFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            StopwatchFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
